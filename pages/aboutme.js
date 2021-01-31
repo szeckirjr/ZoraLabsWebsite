3e87fc464276eab3a0e46bd19ccd0ac537407429
Eduardo Szeckir <eszeckirjr@gmail.com>
2021-01-31 18:26:05 -0300

    Adding more details

--- pages/aboutme.js
+++ pages/aboutme.js
@@ -1,3 +1,21 @@
-export default function FirstPost() {
-    return <h1>First Post</h1>
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+
+export default function AboutMe() {
+  return (
+    <>
+    <Head>
+        <title>Eduardo Szeckir Zora</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    <img src="/profilepic.jpg" alt="Eduardo Szeckir picture"/>
+      <h1 className={styles.title}>About me</h1>
+      <h2 className={styles.description}>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </>
+  )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,28 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Eduardo Szeckir Zora</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
+
+        
+
         <h1 className={styles.title}>
           Hello! My name is <a>Eduardo Szeckir</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Get started by learning{' '}
+          <Link href="aboutme.js">
+            <a>about me!</a>
+          </Link>
         </p>
 
         <div className={styles.grid}>
